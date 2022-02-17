83e4901932e1d668f9ac1ec7f173586dbf606226
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-02-17 16:12:28 -0300

    refactor(client): disable pwa in development

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
+    disable: process.env.NODE_ENV === "development",
     dest: 'public',
     register: true,
     skipWaiting: true,
