5cf7e33d28cb3a79875cd09e591675b8242713b4
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-02-17 19:40:21 -0300

    refactor(client): remove maincontainer and add metatags

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+
 export default function Home() {
   return (
     <>
-      <div></div>
+      <Head>
+        <title>Aline Balas Gourmet - Home</title>
+        <meta
+          name="description"
+          content="Desde 2016 oferecendo uma experiência gourmet, além do habitual, para as balas de coco"
+        />
+        <meta name="distribution" content="Global" />
+        <meta name="owner" content="Aline Roberta Ferreira" />
+        <meta name="creator" content="Diego Ferreira Crivelaro" />
+      </Head>
+      <Header />
     </>
   );
 }
